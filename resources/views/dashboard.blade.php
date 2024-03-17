cb1769d346bc750338557406243944f1ae12b7c7
AndiRasyihan <rasyihanjawahir@iqis.sch.id>
2024-03-18 07:09:33 +0800

    logo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,38 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+    <!-- css bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- our css -->
+    <link rel="stylesheet" href="css/dashboard.css">
+</head>
+<body>
+
+<!-- navbar -->
+<nav class="bg-success">
+<div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item" active><a href="/dashboard" class="nav-link text-white" aria-current="page">Dashboard</a></li>
+        <li class="nav-item"><a href="/tabel" class="nav-link text-white">Tabel Data</a></li>
+        <li class="nav-item"><a href="/tambah" class="nav-link text-white">Tambah Data</a></li>
+        <!-- <li class="nav-item"><a href="#" class="nav-link text-white">FAQs</a></li> -->
+        <li class="nav-item"><a href="/" class="nav-link text-white">log out</a></li>
+      </ul>
+    </header>
+  </div>
+</nav>
+<!-- endnavbar -->
+<div class="container">
+  <h1>Welcome Andi Rasyihan</h1>
+</div>
+</table>
+
+<!-- js bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
